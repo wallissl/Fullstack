ad4bbc86da7e60666ed8262a2515a7b0d5dc97ba
Wallis Silva <wallis.sl@outlook.com>
2023-02-07 22:00:32 -0300

    começo de eventos 07/02/23

---
+++ Modulo01/script.js
@@ -0,0 +1,11 @@
+console.log("Hello Word");
+
+const login = document.getElementById('login');
+
+function clicou () {
+    alert("Esse cara clicou")
+}
+
+login.onclick=clicou;
+
+console.log(login);
